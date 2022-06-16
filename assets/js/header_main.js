6c1ae83da9d4b20b8aad55673b76f1f2a0253b34
Bablu <babla@webcreatoredigitalsolutions.com>
2022-06-16 18:19:14 +0530

    first commit

---
+++ assets/js/header_main.js
@@ -0,0 +1,88 @@
+(function ($) {
+    "use strict";
+
+    new WOW().init();  
+
+    /*---background image---*/
+	function dataBackgroundImage() {
+		$('[data-bgimg]').each(function () {
+			var bgImgUrl = $(this).data('bgimg');
+			$(this).css({
+				'background-image': 'url(' + bgImgUrl + ')', // + meaning concat
+			});
+		});
+    }
+    
+    $(window).on('load', function () {
+        dataBackgroundImage();
+    });
+    
+    /*---stickey menu---*/
+    $(window).on('scroll',function() {    
+           var scroll = $(window).scrollTop();
+           if (scroll < 100) {
+            $(".sticky-header").removeClass("sticky");
+           }else{
+            $(".sticky-header").addClass("sticky");
+           }
+    });
+    
+
+    
+  
+    
+    /*---mini cart activation---*/
+    $('.cart_link > a').on('click', function(){
+        $('.mini_cart,.off_canvars_overlay').addClass('active')
+    });
+    
+    $('.mini_cart_close > a,.off_canvars_overlay').on('click', function(){
+        $('.mini_cart,.off_canvars_overlay').removeClass('active')
+    });
+    
+    
+    
+     /*---canvas menu activation---*/
+    $('.canvas_open').on('click', function(){
+        $('.Offcanvas_menu_wrapper,.off_canvars_overlay').addClass('active')
+    });
+    
+    $('.canvas_close,.off_canvars_overlay').on('click', function(){
+        $('.Offcanvas_menu_wrapper,.off_canvars_overlay').removeClass('active')
+    });
+    
+    
+    /*---Off Canvas Menu---*/
+    var $offcanvasNav = $('.offcanvas_main_menu'),
+        $offcanvasNavSubMenu = $offcanvasNav.find('.sub-menu');
+    $offcanvasNavSubMenu.parent().prepend('<span class="menu-expand"><i class="fa fa-angle-down"></i></span>');
+    
+    $offcanvasNavSubMenu.slideUp();
+    
+    $offcanvasNav.on('click', 'li a, li .menu-expand', function(e) {
+        var $this = $(this);
+        if ( ($this.parent().attr('class').match(/\b(menu-item-has-children|has-children|has-sub-menu)\b/)) && ($this.attr('href') === '#' || $this.hasClass('menu-expand')) ) {
+            e.preventDefault();
+            if ($this.siblings('ul:visible').length){
+                $this.siblings('ul').slideUp('slow');
+            }else {
+                $this.closest('li').siblings('li').find('ul:visible').slideUp('slow');
+                $this.siblings('ul').slideDown('slow');
+            }
+        }
+        if( $this.is('a') || $this.is('span') || $this.attr('clas').match(/\b(menu-expand)\b/) ){
+        	$this.parent().toggleClass('menu-open');
+        }else if( $this.is('li') && $this.attr('class').match(/\b('menu-item-has-children')\b/) ){
+        	$this.toggleClass('menu-open');
+        }
+    });
+    
+    
+    /*js ripples activation*/
+    $('.js-ripples').ripples({
+		resolution: 512,
+		dropRadius: 20,
+		perturbance: 0.04
+	});
+    
+})(jQuery);	
